0061011e46a7a7af2b80952529e026172c565cb1
Nathan Osti <nathanosti.job@gmail.com>
2021-12-22 00:36:13 -0300

    ✨ Create a file to export all assets

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import AnswerSVG from './answer.svg';
+import CheckSVG from './check.svg';
+import CopySVG from './copy.svg';
+import DeleteSVG from './delete.svg';
+import EmptyQuestionsSVG from './empty-questions.svg';
+import GoogleSVG from './google-icon.svg';
+import IllustrationSVG from './illustration.svg';
+import LikeSVG from './like.svg';
+import LogoSVG from './logo.svg';
+
+export {
+  AnswerSVG,
+  CheckSVG,
+  CopySVG,
+  DeleteSVG,
+  EmptyQuestionsSVG,
+  GoogleSVG,
+  IllustrationSVG,
+  LikeSVG,
+  LogoSVG,
+};
